8e9c387ba2329dfba80b2953cfdde5bd725de83f
youngarchi <85197934+youngarchi@users.noreply.github.com>
2022-12-11 17:42:09 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,110 @@
+
+
+//---Swiper
+
+const popularSlider = new Swiper('.swiper', {
+	slidesPerView: 1,
+	spaceBetween: 20,
+	loop: true,
+	
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+
+	breakpoints: {
+		992: {
+			slidesPerView: 3,
+		},
+		660: {
+			slidesPerView: 2,
+		},
+
+		
+	}
+});
+
+
+
+
+
+//Бургер
+
+const menu = document.querySelector('.menu__icon')
+const menuBody = document.querySelector('.menu__body')
+const menuItems = document.querySelectorAll('.menu__item')
+
+if (!(menu.classList.contains("_active") && menuBody.classList.contains("_active"))){
+	menu.addEventListener('click', function(e){
+		document.body.classList.toggle('_lock')
+ 		menu.classList.toggle('_active')
+ 		menuBody.classList.toggle('_active')
+ 		
+	})
+	menuItems.forEach(function(item){
+		item.addEventListener('click', function(){
+			document.body.classList.toggle('_lock')
+			menu.classList.toggle('_active')
+ 			menuBody.classList.toggle('_active')
+		})
+	})
+}
+
+
+
+
+
+
+// if (!(menu.classList.contains("_active") && menuBody.classList.contains("_active"))){
+// 	menu.addEventListener('click', function() {
+// 		document.body.classList.toggle('_lock')
+// 		menu.classList.add('_active')
+// 		menuBody.classList.add('_active')
+// 		console.log(menu.classList)
+			
+// 	})
+// }
+	
+	
+menu
+
+if (menuBody.classList.contains('_active')) {
+	console.log('heyyy')
+	// menu.addEventListener('click', function() {
+	// 	document.body.classList.toggle('_lock')
+	// 	menu.classList.remove('_active')
+	// 	menuBody.classList.remove('_active')
+	// 	console.log('heyyy')
+			
+	// })
+		
+	}
+
+	
+
+
+
+// let menuBurger = document.querySelectorAll('.menu__item')
+// if (menuBurger) {
+// 	if (menu.classList.contains('_active')){
+// 		menuBurger.forEach(function(e){
+// 			e.addEventListener('click', function(){
+// 				menu.classList.remove('_active');
+// 				menuBody.classList.remove('_active');
+// 			})
+// 		})
+
+// 	}
+	
+	
+// }
+
+
+
+
+
+
+
+
